153b6d3e61ad322352286a966cb23cc0304ba4cc
yamathe <manuelsegmar@gmail.com>
2022-08-07 21:10:10 -0500

    ADD: Auth Provider to App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import Signup from "./components/Signup"
+import { Container } from 'react-bootstrap'
+import {AuthProvider} from "./context/AuthContext"
 
 function App() {
   return (
-    <div className="App">asd</div>
+    <AuthProvider>
+      <Container
+        className="d-flex align-items-center justify-content-center"
+        style={{minHeight:"100vh"}}
+      >
+        <div className="w-100" style={{maxWidth:"400px"}}>
+          <Signup/>
+        </div>
+      </Container>
+    </AuthProvider>
   );
 }
 
