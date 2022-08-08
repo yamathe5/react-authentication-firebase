c837af6af8a1ab31f8d82efec731c878571a824c
yamathe <manuelsegmar@gmail.com>
2022-08-07 23:02:52 -0500

    ADD: forgot password page & functions

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 import Dashboard from "./components/Dashboard"
 import Login from "./components/Login"
 import PrivateRoute from "./components/PrivateRoute"
+import ForgotPassword from "./components/ForgotPassword"
 
 function App() {
   return (
@@ -22,6 +23,7 @@ function App() {
 
               <Route path="/signup" element={<Signup/>} />
               <Route path="/login" element={<Login/>} />
+              <Route path="/forgot-password" element={<ForgotPassword/>} />
             </Routes>
           </AuthProvider>
         </Router>
