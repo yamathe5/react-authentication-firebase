17311515536019de0d2c7c4e83c1e30ee6e73888
yamathe <manuelsegmar@gmail.com>
2022-08-07 21:57:58 -0500

    ADD: Dahsboard component with basic navigation

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,7 +1,38 @@
 import React from 'react'
+import {Card, Button, Alert} from "react-bootstrap"
+import { useAuth } from '../context/AuthContext'
+import { Link, useNavigate } from 'react-router-dom'
 
 export default function Dashboard() {
+
+  const [error, setError] =React.useState("")
+  const navigate = useNavigate()
+  const { currentUser, logout } = useAuth()
+
+  async function handleLogout(){
+    
+    setError("")
+    try {
+      await logout()
+      navigate("/login")
+    } catch (error) {
+      setError("Failed to log out")
+    }
+  }
+
   return (
-    <div>Dashboard</div>
+    <>
+    <Card>
+      <Card.Body>
+        <h2 className='text-center mb-4'>Profile</h2>
+        {error && <Alert variant="danger">{error}</Alert >}
+        <strong>Email: </strong>{currentUser.email}
+        <Link to="/update-profile" className="btn btn-primary w-100 mt-3">Update profile</Link>
+      </Card.Body> 
+    </Card>
+    <div className='w-100 text-center mt-2'>
+      <Button variant="link" onClick={handleLogout}>Log Out</Button>
+    </div>
+    </>
   )
 }
